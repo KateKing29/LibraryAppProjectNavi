2f2fc63b7f4901151b49becc94d9af7c89eeb905
Simona <simona.savic88@gmail.com>
2023-08-07 14:43:59 -0400

    added glue steps

--- src/test/java/com/library/runner/CukesRunner.java
+++ src/test/java/com/library/runner/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber"
         },
         features = "src/test/resources/features" ,
-        glue = "com/cydeo/steps",
+        glue = "com/library/step_definitions",
         dryRun = false,
         tags = "@wip"
 )
